95aa1b8cf3e58e4b3eb19e7eead1a15d5e1c3fd4
tesseractT <ebere.benny97@gmail.com>
2023-05-20 18:04:31 +0100

    added admin backend assets

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,11 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
+//Admin Dashboard
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 });
+//Vendor Dashboard
 Route::middleware(['auth', 'role:vendor'])->group(function () {
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
 });
