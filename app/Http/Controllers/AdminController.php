1b43da9aa5609465db7e4f508625112e5a00df9d
tesseractT <ebere.benny97@gmail.com>
2023-05-29 18:21:49 +0100

    toaster done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -49,13 +49,19 @@ class AdminController extends Controller
 
         if ($request->file('photo')) {
             $file = $request->file('photo');
-            @unlink(public_path('upload/admin_images/'.$data->photo));
+            @unlink(public_path('upload/admin_images/' . $data->photo));
             $filename = date('YmdHi') . $file->getClientOriginalName();
             $file->move(public_path('upload/admin_images'), $filename);
             $data['photo'] = $filename;
         }
 
         $data->save();
-        return redirect()->back();
+
+        $notification = [
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success',
+        ];
+
+        return redirect()->back()->with($notification);
     } // End Method
 }
