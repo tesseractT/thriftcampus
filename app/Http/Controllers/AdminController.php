0d3dc9b6f46da7deec604ac411558826e2f49008
tesseractT <ebere.benny97@gmail.com>
2023-05-29 13:39:00 +0100

    customize admin login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
         return view('admin.index');
+    } // End Method
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    }// End Method
+
+    public function AdminDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
     } // End Method
 }
--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,10 @@ require __DIR__ . '/auth.php';
 //Admin Dashboard
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
 });
 //Vendor Dashboard
 Route::middleware(['auth', 'role:vendor'])->group(function () {
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
 });
+Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
